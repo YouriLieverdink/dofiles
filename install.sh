d580613b77ba239e07b4544abb0970e25199ec99
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-01-04 15:31:33 +0100

    feat: create an install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Command line tools
+xcode-select --install
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle --file ~/.dot-files/Brewfile
+
+# Symlinks
+ln -sf ~/.dot-files/system/zshrc ~/.zshrc
+ln -sf ~/.dot-files/config/git/.gitconfig ~/.gitconfig
+ln -sf ~/.dot-files/config/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
